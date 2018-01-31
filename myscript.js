b3cc4395a32e53eedc94ec52035941a1bf7e0adc
ThatGuyChrisZ <thatguynamedchriszinser@gmail.com>
2018-01-31 09:20:40 -0800

    The beginning

---
+++ myscript.js
@@ -0,0 +1,90 @@
+document.getElementById("myButton").addEventListener("click", myfunction)
+document.getElementById("input2").addEventListener("click", myfunction3)
+document.getElementById("input2").addEventListener("click", myfunction3)
+document.getElementById("myButton3").addEventListener("click", myfunction5)
+                                                     
+var webview = document.getElementById('foo');
+var para = document.createElement("webview");
+var para2 = document.createElement("div1");
+var para3 = document.getElementById("foo");
+var element = document.getElementById("div1");
+var x = document.getElementsByName("iframe");
+
+    
+
+
+
+
+
+function myfunction(){
+    setTimeout(myfunction2, 200)
+
+document.getElementById('foo').setAttribute("src","google.com")
+document.getElementById('foo').setAttribute("src","google.com")
+document.getElementById('myButton').appendChild(webview);
+document.getElementById('foo').parentNode.removeChild(webview)
+para.setAttribute("src", "https://www.google.com/")
+para.setAttribute("width", "auto")
+para.setAttribute("id", "foo")
+
+
+function myfunction2(){
+    para.setAttribute("width", "730")
+  
+    console.log(para3.children);
+     var z = document.getElementById("foo").childNodes.length;
+    console.log(z)
+     var c = document.getElementById("foo").childNodes;
+  //  c[0].style.width = "200%"
+    para.style.width = "720px"
+    para.style.height = "500px"
+  
+   
+}
+
+
+element.appendChild(para2);
+element.appendChild(para);
+    
+
+    
+
+}
+function myfunction3(){
+    setTimeout(myfunction4, 200)
+var search2 = document.getElementById("input").value
+var search = "https://www." + search2
+console.log(search)
+document.getElementById('foo').setAttribute("src",search)
+document.getElementById('foo').setAttribute("src",search)
+document.getElementById('myButton').appendChild(webview);
+document.getElementById('foo').parentNode.removeChild(webview)
+para.setAttribute("src",search)
+para.setAttribute("width", "auto")
+para.setAttribute("id", "foo")
+
+
+function myfunction4(){
+    para.setAttribute("width", "730")
+  
+    console.log(para3.children);
+     var z = document.getElementById("foo").childNodes.length;
+    console.log(z)
+     var c = document.getElementById("foo").childNodes;
+  //  c[0].style.width = "200%"
+    para.style.width = "720px"
+    para.style.height = "500px"
+  
+   
+}
+
+
+element.appendChild(para2);
+element.appendChild(para);
+    
+
+    
+
+}
+function myfunction5(){webview.reload()}
+     
